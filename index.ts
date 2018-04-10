99efa9485e836a3944e0b7d29143c18f3b41f857
Silvelo <arturo.silvelo@gmail.com>
2018-04-10 23:28:44 +0200

    Update example with new package

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { IAction, TelegramActions } from '@telegram-actions/core';
 import { transmissionActions } from '@telegram-actions/transmission';
+import { systemActions } from '@telegram-actions/system';
 
 const token = process.env.TOKEN || '';
 
 const telegramActions = new TelegramActions(token, true);
 
 telegramActions.addActions(transmissionActions);
+telegramActions.addActions(systemActions);
 telegramActions.start();
